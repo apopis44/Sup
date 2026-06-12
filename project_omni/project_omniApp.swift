ed6cd403e296e324214895a2e9e497b71a2898f7
swaroop <swaroopvontela@gmail.com>
2026-06-12 20:12:24 +0530

    Initial Commit

---
+++ project_omni/project_omniApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_omniApp.swift
+//  project_omni
+//
+//  Created by swaroop reddy vontela on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct project_omniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
